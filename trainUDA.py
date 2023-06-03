20ee9871db17e4bbd8579f4272f6c4b1ceb741bd
DuongKien2001 <trungkienty2001@gmail.com>
2023-06-03 07:37:15 +0700

    k

--- trainUDA.py
+++ trainUDA.py
@@ -612,7 +612,8 @@ def main():
                 tgt_mask_upt[(((max_probs_65 < cfg.SOLVER.DELTA) * (targets_u_w_65 == i)).int() + (pseudo_weight != 1.0).int()) == 2] = 255
             if i_iter < 10020:
                 print((pseudo_weight != 1.0).sum())
-                print((tgt_mask_upt == 255).sum())
+                for i in range(cfg.MODEL.NUM_CLASSES):
+                    print((tgt_mask_upt == 255).sum())
             tgt_mask_upt = tgt_mask_upt.contiguous().view(B * Hs * Ws, )
             src_feat = src_feat.permute(0, 2, 3, 1).contiguous().view(B * Hs * Ws, A)
             tgt_feat = tgt_feat.permute(0, 2, 3, 1).contiguous().view(B * Ht * Wt, A)
